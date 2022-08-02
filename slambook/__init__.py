2037e66f435c237f0823f17739317061ae49afb2
unknown <nellaipranav@gmail.com>
2022-08-02 21:05:58 +0530

    third commit

--- slambook/__init__.py
+++ slambook/__init__.py
@@ -9,7 +9,7 @@ app  = Flask(__name__)
 app.config['SECRET_KEY'] = 'f8e6c5b914302b637b9130dfa0498389'
  
  
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fzuxyucaqzxqhx:a461bd0673c24485cc0c21ed5b5374eb45ede018a39722657968e3f8f9c838b7@ec2-44-209-186-51.compute-1.amazonaws.com:5432/ddfaktscvpnce5'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fzuxyucaqzxqhx:a461bd0673c24485cc0c21ed5b5374eb45ede018a39722657968e3f8f9c838b7@ec2-44-209-186-51.compute-1.amazonaws.com:5432/ddfaktscvpnce5'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
