f64d5399c9fb5b732dd8b1c5a6a3719dd99598ec
unknown <nellaipranav@gmail.com>
2022-08-04 23:44:04 +0530

    fourth commit

--- slambook/__init__.py
+++ slambook/__init__.py
@@ -9,7 +9,7 @@ app  = Flask(__name__)
 app.config['SECRET_KEY'] = 'f8e6c5b914302b637b9130dfa0498389'
  
  
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://qurugnbrvjnnvx:bca1f1255d29dd433d3534b69bec322a27389f19722a21d93f81161773e2adcf@ec2-107-22-122-106.compute-1.amazonaws.com:5432/ddfg2o90610lq7'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
