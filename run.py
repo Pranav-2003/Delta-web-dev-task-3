f61472da05ea9a8703a97c0250b74145266c92df
unknown <nellaipranav@gmail.com>
2022-08-11 20:45:35 +0530

    ui improvements

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 from slambook import app,db
 #from slambook.models import Comment, Post, User
-
 if(__name__=="__main__"):
     app.run(debug=True)
 
--- slambook/models.py
+++ slambook/models.py
@@ -10,7 +10,7 @@ def load_user(user_id):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     department = db.Column(db.String(7), nullable=False)
     batch = db.Column(db.String(4), nullable=False)
