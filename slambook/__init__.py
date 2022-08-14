8b24d0916e8d3e4eb348dd8fd91321a946c14e37
unknown <nellaipranav@gmail.com>
2022-08-14 20:13:02 +0530

    bug fix

--- slambook/__init__.py
+++ slambook/__init__.py
@@ -9,7 +9,7 @@ app  = Flask(__name__)
 app.config['SECRET_KEY'] = 'f8e6c5b914302b637b9130dfa0498389'
  
  
-app.config['SQLALCHEMY_DATABASE_URI'] = ' postgresql://nlndqtovnrgeke:763f97bafed23ff10e55c07a0b7bc549882fae6b473f776e9d0be1ce2985f52a@ec2-34-203-182-65.compute-1.amazonaws.com:5432/d9tp38roctgdpj'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://nlndqtovnrgeke:763f97bafed23ff10e55c07a0b7bc549882fae6b473f776e9d0be1ce2985f52a@ec2-34-203-182-65.compute-1.amazonaws.com:5432/d9tp38roctgdpj'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
